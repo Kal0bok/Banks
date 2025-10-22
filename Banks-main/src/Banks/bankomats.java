5cd73b92fef6a29ac08e313a89dd6b9d90c3965d
Karasik <nikitatt3914@gmail.com>
2025-10-22 15:09:10 +0300

    asd
    asd

---
+++ Banks-main/src/Banks/bankomats.java
@@ -0,0 +1,16 @@
+package Banks;
+
+public class bankomats {
+
+	public static void main(String[] args) {
+		String[]darbibas = {"Jauna karte","Noņemt karte",
+				"kartu saraksts","Izsaukt metode",
+				"Aizvērt programmu"};
+		String[]veidi = {"Noreķinu karte", "",
+		"Bernu velosipēds"};
+			
+		
+
+	}
+
+}
